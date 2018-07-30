a9d1cddad200b3d484d9c3e0786a6b422da54176
Joseph Gabito <codehaiku.io@gmail.com>
2018-07-30 21:25:03 +0800

    new update

--- single.php
+++ single.php
@@ -9,12 +9,10 @@
 
 get_header();
 ?>
-
-	
 <div class="container">
 	<div class="row">
 		<div class="col-sm-12 col-md-12 col-lg-8">
-			<div id="primary" class="content-area">
+			<div id="primary" class="content-area py-5">
 				<main id="main" class="site-main">
 
 				<?php
@@ -37,7 +35,7 @@ get_header();
 			</div><!-- #primary -->
 		</div>
 		<div class="col-sm-12 col-md-12 col-lg-4">
-			<div class="sidebar">
+			<div class="sidebar py-5">
 				<?php get_sidebar(); ?>
 			</div>
 		</div>
