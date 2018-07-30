bfef6469a689d657416e21d079062884bb2f77e7
Joseph Gabito <codehaiku.io@gmail.com>
2018-07-30 16:45:36 +0800

    added first header style

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  	<div class="form-group">
+    	<input type="search" name="s" class="mr-sm-2 form-control" 
+    	placeholder="<?php echo esc_attr('Search', 'nibble-core'); ?>">
+    	<button class="btn btn-outline-success" type="submit">Search</button>
+  	</div>
+</form>
