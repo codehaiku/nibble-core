33a78593d2d059005d3d8eae6000f0e569181faf
Joseph Gabito <codehaiku.io@gmail.com>
2018-07-31 15:02:40 +0800

    Template comments

--- single.php
+++ single.php
@@ -21,7 +21,11 @@ get_header();
 
 					get_template_part( 'template-parts/content', get_post_type() );
 
-					the_post_navigation();
+					?>
+					<div class="mb-3">
+						<?php the_post_navigation(); ?>
+					</div>
+					<?php
 
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
