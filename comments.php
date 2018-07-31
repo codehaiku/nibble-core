a78120d542269c82049d892dfeb6779b4f7fa644
Joseph Gabito <codehaiku.io@gmail.com>
2018-07-31 17:15:22 +0800

    new update

--- comments.php
+++ comments.php
@@ -21,22 +21,20 @@ if ( post_password_required() ) {
 ?>
 
 <div id="comments" class="comments-area">
-
+	<h2 class="comments-title h4 mt-4 mb-5 border-bottom border-dark">
+		<span class="bg-dark text-light d-inline-block px-3 py-2">
+			<i class="far fa-comments"></i>
+			<?php 
+			comments_number( __('Leave a Comment','nibble-core'), 
+			__('One Comment', 'nibble-core'), __('% Comments', 'nibble-core') ); 
+			?>
+		</span>
+	</h2><!-- .comments-title -->
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title h4 mt-4 mb-5 border-bottom border-dark">
-			<span class="bg-dark text-light d-inline-block px-3 py-2">
-				<i class="far fa-comments"></i>
-				<?php 
-				comments_number( __('Add Comment','nibble-core'), 
-				__('One Comment', 'nibble-core'), __('% Comments', 'nibble-core') ); 
-				?>
-			</span>
-		</h2><!-- .comments-title -->
-
-		<?php the_comments_navigation(); ?>
+		
+		the_comments_navigation(); ?>
 
 		<ol class="comment-list list-unstyled">
 			<?php
@@ -62,9 +60,39 @@ if ( post_password_required() ) {
 
 	endif; // Check for have_comments().
 
-		comment_form($args=array(
-			'class_submit' => 'btn btn-primary'
-		));
+		$commenter = wp_get_current_commenter();
+		$req = get_option( 'require_name_email' );
+		$aria_req = ( $req ? " aria-required='true'" : '' );
+
+		$fields =  array(
+		  'author' =>
+		    '<div class="comment-form-author form-group"><label for="author">' . __( 'Name', 'nibble-core' ) .
+		    ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
+		    '<input class="form-control" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+		    '" size="30"' . $aria_req . ' /></div>',
+
+		  'email' =>
+		    '<div class="comment-form-email form-group"><label for="email">' . __( 'Email', 'nibble-core' ) .
+		    ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
+		    '<input class="form-control" id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+		    '" size="30"' . $aria_req . ' /></div>',
+
+		  'url' =>
+		    '<div class="comment-form-url form-group"><label for="url">' . __( 'Website', 'nibble-core' ) . '</label>' .
+		    '<input class="form-control" id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+		    '" size="30" /></div>',
+		);
+		
+		$comment_field = '<div class="form-group comment-form-comment"><label for="comment">' . esc_html__( 'Comment', 'nibble-core' ) . '</label><textarea class="form-control" required id="comment" name="comment" cols="45" rows="4" aria-required="true"></textarea></div>';
+
+		comment_form(
+			array(
+				'class_submit' => 'btn btn-primary',
+				'comment_field' => apply_filters('nibble_core_comment_field', $comment_field),
+				'must_login' => '<p class="must-log-in text-info">' .  sprintf( __( 'You must be <a href="%s">logged in</a> to post a comment.' ), wp_login_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</p>',
+				'fields' => apply_filters( 'comment_form_default_fields', $fields )
+			)
+		);
 	?>
 
 </div><!-- #comments -->
