d6b81353ff27f3ac85f1fb277c4d43b91f60cc22
Joseph Gabito <codehaiku.io@gmail.com>
2018-07-31 15:02:40 +0800

    Template comments

--- index.php
+++ index.php
@@ -15,47 +15,42 @@
 get_header();
 $layout = '2';
 ?>
-
-
 <div class="container">
 	<div class="row">
 		<div class="col-sm-12 col-md-12 col-lg-8">
 			<div id="primary" class="content-area py-5">
 				<main id="main" class="site-main">
-
 				<?php
 				if ( have_posts() ) :
-
 					if ( is_home() && ! is_front_page() ) :
 						?>
 						<header>
-							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+							<h1 class="page-title screen-reader-text">
+								<?php single_post_title(); ?>
+							</h1>
 						</header>
 						<?php
 					endif;
-
 					/* Start the Loop */
 					while ( have_posts() ) :
 						the_post();
-
 						/*
 						 * Include the Post-Type-specific template for the content.
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 						 */
 						get_template_part( 'template-parts/content', get_post_type() );
-
 					endwhile;
-
-					the_posts_navigation();
-
+					/* Post Navigation */
+					?>
+					<div class="d-flex justify-content-center">
+						<?php the_posts_navigation(); ?>
+					</div>
+					<?php
 				else :
-
 					get_template_part( 'template-parts/content', 'none' );
-
 				endif;
 				?>
-
 				</main><!-- #main -->
 			</div><!-- #primary -->
 		</div>
