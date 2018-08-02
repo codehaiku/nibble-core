18e0a91f2d46f6d1c5c65bfac2a67026502993c2
Joseph Gabito <codehaiku.io@gmail.com>
2018-08-02 11:17:14 +0800

    new update

--- page.php
+++ page.php
@@ -15,36 +15,39 @@
 get_header();
 ?>
 
-	
-
+<div class="page-header bg-light border-bottom mb-5">
+	<div class="container">
+		<div id="page-title" class="py-4">
+			<?php the_title('<h1 class="entry-title mb-0">','</h1>'); ?>
+			<?php if ( function_exists( 'nibble_core_the_page_leading_text') ): ?>
+				<?php nibble_core_the_page_leading_text(); ?>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
 <div class="container">
 	<div class="row">
-		<div class="col-sm-12 col-md-12 col-lg-8">
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main">
-
-				<?php
-				while ( have_posts() ) :
-					the_post();
-
-					get_template_part( 'template-parts/content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-
-				endwhile; // End of the loop.
-				?>
-
-				</main><!-- #main -->
-			</div><!-- #primary -->
-		</div>
+		<?php while( have_posts() ) : ?>
+			<?php the_post(); ?>
+			<div class="col-sm-12 col-md-12 col-lg-8">
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main">
+						<?php get_template_part( 'template-parts/content', 'page' ); ?>
+						<?php 
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+						?>
+					</main>
+				</div>
+			</div>
+		<?php endwhile; ?>
 		<div class="col-sm-12 col-md-12 col-lg-4">
 			<?php get_sidebar(); ?>
 		</div>
 	</div>
-</div>
-<?php
+</div>	
+<?php 
 
 get_footer();
