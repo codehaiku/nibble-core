d35e400835559b4ee7604331ae468c2b24e1cf39
Joseph Gabito <codehaiku.io@gmail.com>
2018-08-02 19:14:42 +0800

    new update

--- js/nibble-core.js
+++ js/nibble-core.js
@@ -1,5 +1,23 @@
 (function($){
+	
 	'use strict';
+
 	$('[data-toggle="tooltip"]').tooltip();
 	$('.page-item > a').addClass('page-link');
+
+	$('figure.gallery-item').on('mouseover', function(){
+		//$(this).children('figcaption.wp-caption').css('top', '5px');
+		
+		var caption = $(this).find('.wp-caption-text');
+		if ( caption.length >= 1 ) {
+			console.log('caption found')
+			var xCenter = ($(this).outerWidth() - caption.outerWidth())/2;
+			var yTop = $(this).height() + 15;
+			caption.css({
+				top: yTop + 'px',
+				left: (xCenter - 2) + 'px'
+			});
+		}
+	});
+
 })(jQuery);
